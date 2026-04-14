cd3b8abb1cf56ca0e1ab6013e5a26c57490a3552
martin2025santos-ops <martin2025santos@gmail.com>
2026-04-14 15:34:20 -0300

    Delete index (1) directory

--- index (1)/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-const player = new Plyr('#my-video', {
-controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen',
-'download'],
-});
-});
